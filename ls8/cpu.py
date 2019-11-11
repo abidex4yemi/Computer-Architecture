0313d09282086ea4facbc49f4e322cb30f7913e7
abidex4yemi <abidex4yemi@gmail.com>
2019-11-11 15:32:50 +0100

    Add list properties to CPU constructor

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,13 +1,23 @@
-"""CPU functionality."""
+# CPU functionality.
 
 import sys
 
-class CPU:
-    """Main CPU class."""
 
+class CPU:
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        # 8 general-purpose registers
+        self.register = [0] * 8
+
+        # hold 256 bytes of memory
+        self.memory = [0] * 255
+
+        self.pc = 0
+
+    def ram_read(self, address):
+        return self.memory_data_register[address]
+
+    def ram_write(self, value):
+        return self.memory_data_register[value] = value
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +28,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +57,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
